845bf51caf4591c150828ccd33335d16bad75706
Nugroho Dewantorto <nugroho.dewantoro@gmail.com>
2023-08-03 04:39:51 +0700

    fix vup

--- src/engine/utils/camera/camera.hpp
+++ src/engine/utils/camera/camera.hpp
@@ -14,8 +14,8 @@ namespace nugiEngine {
     public:
       EngineCamera(uint32_t width, uint32_t height);
 
-      void setViewDirection(glm::vec3 position, glm::vec3 direction, float vfov, glm::vec3 up = glm::vec3{0.0f, -1.0f, 0.0f});
-      void setViewTarget(glm::vec3 position, glm::vec3 target, float vfov, glm::vec3 up = glm::vec3{0.0f, -1.0f, 0.0f});
+      void setViewDirection(glm::vec3 position, glm::vec3 direction, float vfov, glm::vec3 up = glm::vec3{0.0f, 1.0f, 0.0f});
+      void setViewTarget(glm::vec3 position, glm::vec3 target, float vfov, glm::vec3 up = glm::vec3{0.0f, 1.0f, 0.0f});
 
       CameraRay getCameraRay() const { return this->cameraRay; }
 
